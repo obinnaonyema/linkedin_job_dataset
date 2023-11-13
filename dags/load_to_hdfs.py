730ec12f01716b896959ad0e28eeea4f657d42aa
Obinna Onyema <oeonyema@yahoo.com>
2023-11-12 22:42:45 -0500

    first sftp upload

--- dags/load_to_hdfs.py
+++ dags/load_to_hdfs.py
@@ -2,9 +2,12 @@
 import airflow
 from airflow import DAG
 from airflow.contrib.operators.ssh_operator import SSHOperator
+from airflow.operators.bash_operator import BashOperator
+from airflow.providers.sftp.operators.sftp import SFTPOperator
+
 
 bash_commands = """
-cp /opt/airflow/data/* /home/data/lab/
+put file:///opt/airflow/data/benefits.csv /home/data/lab/
 """
 
 args = {"owner": "Group3", "start_date": airflow.utils.dates.days_ago(2)}
@@ -14,11 +17,27 @@ dag = DAG(
 )
 
 with dag:
-    task1 = SSHOperator(
-        ssh_conn_id = 'AzureVM',
-        task_id = 'Connect_to_VM',
-        command = bash_commands
+    # task1 = SSHOperator(
+    #     ssh_conn_id = 'AzureVM',
+    #     task_id = 'Connect_to_VM',
+    #     command = bash_commands
+    # )
+
+    run_this = BashOperator(
+    task_id="run_after_loop",
+    bash_command='ls -la /opt/airflow/plugins/upload/',
     )
+
+    taskSFTP = SFTPOperator(
+    task_id="test_sftp",
+    ssh_conn_id="AzureVM",
+    local_filepath="/opt/airflow/plugins/upload/companies.csv",
+    remote_filepath="/home/data/lab/companies.csv",
+    operation="put",
+    create_intermediate_dirs=True
+    )
+
+
 
 
 
